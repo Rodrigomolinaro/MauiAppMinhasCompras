1835fbf1b4ddab0d1dfacbc92bd615352a9d0cb6
RODRIGO MOLINARO <rodrigomolinaro@live.com>
2026-08-22 13:48:20 -0300

    Correção da parte Quantidade que não estava aparecendo

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -17,13 +17,15 @@ public partial class NovoProduto : ContentPage
 			{
 				Descricao = txt_descricao.Text,
 				Quantidade = Convert.ToDouble(txt_quantidade.Text),
-				Preco = Convert.ToDouble(txt_preco.Text)
+				Preco = Convert.ToDouble(txt_preco.Text),
 			};
 
 			await App.Db.Insert(p);
 			await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
 
-		} catch(Exception ex)
+            await Navigation.PopAsync();
+
+        } catch(Exception ex)
 		{
 			await DisplayAlert("Ops", ex.Message, "OK");
 		}
